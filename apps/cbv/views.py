047edc4cad28bba3a372c525acfc10786130b2fd
devbaggett <devabaggett@gmail.com>
2018-08-03 16:24:10 -0700

    added CRUD operations

--- apps/cbv/views.py
+++ apps/cbv/views.py
@@ -1,16 +1,34 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView, ListView, DetailView
+from django.views.generic import View, TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 from . import models
+from django.urls import reverse_lazy
 
 
 class IndexView(TemplateView):
-	template_name = 'index.html'
+    template_name = 'index.html'
+
 
 class SchoolListView(ListView):
-	context_object_name = 'schools'
-	model = models.School
+    context_object_name = 'schools'
+    model = models.School
+
 
 class SchoolDetailView(DetailView):
-	context_object_name = 'school_detail'
+    context_object_name = 'school_detail'
+    model = models.School
+    template_name = 'cbv/school_detail.html'
+
+class SchoolCreateView(CreateView):
+	fields = ('name', 'principal', 'location')
 	model = models.School
-	template_name = 'cbv/school_detail.html'
+
+class SchoolUpdateView(UpdateView):
+	# limit what fields you can update
+	fields = ('name', 'principal')
+	model = models.School
+
+class SchoolDeleteView(DeleteView):
+	model = models.School
+	# After successful delete, go back to list page and show all schools minus recently deleted
+	success_url = reverse_lazy("cbv:list")
+
